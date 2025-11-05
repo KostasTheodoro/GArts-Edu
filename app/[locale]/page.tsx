f126e45a9bdfe32ee0f7ce81497abe67096de5e5
KostasTheodoro <98785109+KostasTheodoro@users.noreply.github.com>
2025-11-05 20:58:34 +0200

    final hero decision

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,11 +1,6 @@
 import type { Metadata } from "next";
 import { getTranslations } from "next-intl/server";
-import Hero from "./components/Hero";
-import HeroAnimated1 from "./components/HeroAnimated1";
-import HeroAnimated2 from "./components/HeroAnimated2";
-import HeroAnimated3 from "./components/HeroAnimated3";
 import HeroBlended from "./components/HeroBlended";
-import HeroBlended3 from "./components/HeroBlended3";
 import HowWeThink from "./components/HowWeThink";
 
 export async function generateMetadata({
@@ -25,12 +20,7 @@ export async function generateMetadata({
 export default function HomePage() {
   return (
     <main className="w-full">
-      <Hero />
-      <HeroAnimated1 />
-      <HeroAnimated2 />
-      <HeroAnimated3 />
       <HeroBlended />
-      <HeroBlended3 />
       <HowWeThink />
     </main>
   );
